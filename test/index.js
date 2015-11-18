7c7f614d11d54197b5e827c4dedfd81b98e2f26a
Choi KyuWoo <apple@paio.co.kr>
2015-11-19 07:30:33 +0900

    fix lint. add tests.

--- test/index.js
+++ test/index.js
@@ -1,9 +1,81 @@
 import assert from 'assert';
-import coMediator from '../lib/index';
+import CoMediator from '../lib/index';
 
 describe('co-mediator', function () {
-  it('should have unit test!', function () {
-    coMediator.test();
-    assert(true, 'we expected this package author to add actual unit tests!');
+
+  it('thrown error on subscribed callback function', function (done) {
+    let cm = new CoMediator();
+    let error = 'an error';
+    cm.subscribe('test', function* () {
+      throw error;
+    }, function (e) {
+      assert(e === error, 'must be caught.');
+      done();
+    });
+    cm.publish('test');
+  });
+
+  it('thrown error on subscribed once callback function', function (done) {
+    let cm = new CoMediator();
+    let error = 'an error';
+    cm.subscribeOnce('test', function* () {
+      throw error;
+    }, function (e) {
+      assert(e === error, 'must be caught.');
+      done();
+    });
+    cm.publish('test');
+  });
+
+  it('subscribed callback function', function (done) {
+    let cm = new CoMediator();
+    let testData = 'a string';
+    cm.subscribe('test', function (publishedData) {
+      assert(testData === publishedData, 'should be called with passed data');
+      done();
+    }, function (e) {
+      done(e);
+    });
+    cm.publish('test', testData);
+  });
+
+  it('subscribed generator callback function', function (done) {
+    let cm = new CoMediator();
+    let testData = 'a string';
+    cm.subscribe('test', function* (publishedData) {
+      assert(testData === publishedData, 'should be called with passed data');
+      let a = yield Promise.resolve(1);
+      assert(a === 1, 'should be called as co-generator-function');
+      done();
+    }, function (e) {
+      done(e);
+    });
+    cm.publish('test', testData);
+  });
+
+  it('subscribed once callback function', function (done) {
+    let cm = new CoMediator();
+    let testData = 'a string';
+    cm.subscribeOnce('test', function (publishedData) {
+      assert(testData === publishedData, 'should be called with passed data');
+      done();
+    }, function (e) {
+      done(e);
+    });
+    cm.publish('test', testData);
+  });
+
+  it('subscribed once generator callback function', function (done) {
+    let cm = new CoMediator();
+    let testData = 'a string';
+    cm.subscribeOnce('test', function* (publishedData) {
+      assert(testData === publishedData, 'should be called with passed data');
+      let a = yield Promise.resolve(1);
+      assert(a === 1, 'should be called as co-generator-function');
+      done();
+    }, function (e) {
+      done(e);
+    });
+    cm.publish('test', testData);
   });
 });
